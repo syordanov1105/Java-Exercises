41012733342673831765fffcd6e1ea534ee646d2
Stanislav Y <syordanov1105@gmail.com>
2023-09-08 15:53:51 +0300

    Implement Abstract Class

---
+++ OOP/Exercises on Polymorphism, Abstract Classes and Interfaces/AbstarctSuperClass/Shape.java
@@ -0,0 +1,41 @@
+abstract class Shape {
+    String color;
+    Boolean filled;
+
+    public Shape() {
+        this.color = "red";
+        this.filled = true;
+    }
+
+    public Shape(String color, Boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getFilled() {
+        return filled;
+    }
+
+    public void setFilled(Boolean filled) {
+        this.filled = filled;
+    }
+
+    abstract double getArea();
+    abstract double getPerimeter();
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
